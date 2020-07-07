14db7b45f0809ac2aad0bad37cf7ab3b46e27999
vinodh-ui <rogerjohn452@gmail.com>
2020-07-07 21:10:24 +0800

    Filtercommit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,84 @@
 ///featur-1
-import React from 'react';
-import logo from './logo.svg';
-import data from './data.json'
-import './App.css';
-import Products from './Componenets/Products'
+import React from "react";
+import logo from "./logo.svg";
+import data from "./data.json";
+import "./App.css";
+import Products from "./Componenets/Products";
+import Filter from "./Componenets/Filter";
 
-class App   extends React.Component{
+class App extends React.Component {
   constructor(props) {
-    super(props)
-  
-    this.state = {
-       
-Products:data.products,
-sort:'',
-size:''
+    super(props);
 
+    this.state = {
+      Products: data.products,
+      sort: "",
+      size: "",
+    };
+  }
+
+  Sortproducts = (event) => {
+    console.log(event.target.value);
+
+    if (event.target.value === "") {
+      this.setState({ Products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        Products: data.products.filter((pr) =>
+          pr.availableSizes.includes(event.target.value)
+        ),
+      });
+    }
+  };
+
+
+
+  filterproducts = (e) => {
+    if (e.target.value === "lowest") {
+      this.setState({
+        sort: e.target.value,
+        Products: data.products.sort((a, b) => (a.price > b.price ? 1 : -1)),
+      });
+    } else if (e.target.value === "highest") {
+      this.setState({
+        sort: e.target.value,
+        Products: data.products.reverse((a, b) => (b.price > a.price ? 1 : -1)),
+      });
+    } else if (e.target.value === "latest") {
+
+      this.setState({
+
+        sort: e.target.value,
+       Products:data.products.sort((a,b) => a._id > b._id ? 1: -1)
+
+      });
 
     }
-  }
-  render(){
-
-
-  return (
-    <div className="App ">
-
-
-
-<Products products={this.state.Products}></Products>
-
-
    
-    </div>
-  );
+  };
+
+
+
+  render() {
+    return (
+      <div className="App ">
+        <Filter
+          count={this.state.Products.length}
+          size={this.state.size}
+          sort={this.state.sort}
+          filterproducts={this.filterproducts}
+          Sortproducts={this.Sortproducts}
+        >
+          {" "}
+        </Filter>
+
+        <Products
+          products={this.state.Products}
+          Pr={this.state.Products.length}
+        ></Products>
+      </div>
+    );
   }
 }
 
--- src/Componenets/Products.js
+++ src/Componenets/Products.js
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
 
- class Products extends Component {
+class Products extends Component {
   render() {
     return (
       <div className="cc">
-          
-        {this.props.products.map((Product) => {
-          return <div key={Product._id}>
-<h1>{Product.title}</h1>
-<img src={Product.image} alt={Product.title}/>
-<button>Add cart</button>
+        {this.props.Pr === 0 ? (
+          "No products found for  your size try  some other sizes"
+        ) : (
+          <div>
+            {this.props.products.map((Product) => {
+              return (
+                <div key={Product._id}>
+                  <h1>{Product.title}</h1>
+                  <img src={Product.image} alt={Product.title} />
+                  {Product.availableSizes}
+                  <h4>Price: {Product.price}</h4>
 
-
-
-
-          </div>;
-        })}
-
-        
+                  <button>Add cart</button>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
 }
-export default Products
+export default Products;
